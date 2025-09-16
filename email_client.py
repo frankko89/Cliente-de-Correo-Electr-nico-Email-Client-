7e4873edd0986891584e78e555a07130e63edd65
Aainor <orianacasasardone@gmail.com>
2025-09-16 19:13:35 -0300

    base de servidorCorreo y Carpeta, a realizar por Franco

--- email_client.py
+++ email_client.py
@@ -1,4 +1,10 @@
 from user import Usuario 
+
+class servidorCorreo():
+    pass
+
+class Carpeta(servidorCorreo):
+    pass
         
 user1 = Usuario("Oriana", "Casas", "oriana@gmail.com")
 user2 = Usuario("Franco", "Bishalba", "elfran@gmail.com")
